1b356e3a5ba2c39e8c4042c99f61d569cf44245c
AtomicBoolean <snow.benjamin2@gmail.com>
2023-08-18 12:54:09 +0200

    Small typo.

--- src/plugins/base/init.ts
+++ src/plugins/base/init.ts
@@ -3,7 +3,7 @@
  * @description The plugins only contains some base events.
  * A global event is an event functionnality that should not be used in multiples plugins
  * as it can cause some bugs.
- * Take as an example the 'interactionCreate' event, if multiple plugings
+ * Take as an example the 'interactionCreate' event, if multiple plugins
  * executes the bot's commands, then some errors will occurs as multiples
  * functions will try to reply to the same interaction.
  *
